8e1347f1c73380e46bc2de6717540f9153161eb3
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-10-17 21:59:48 +0200

    v0.0.5

--- dist/magus-library.js
+++ dist/magus-library.js
@@ -1,51 +1,61 @@
-function _array_like_to_array(arr, len) {
-    if (len == null || len > arr.length) len = arr.length;
-    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
-    return arr2;
-}
-function _array_without_holes(arr) {
-    if (Array.isArray(arr)) return _array_like_to_array(arr);
-}
-function _iterable_to_array(iter) {
-    if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
-}
-function _non_iterable_spread() {
-    throw new TypeError("Invalid attempt to spread non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
-}
-function _to_consumable_array(arr) {
-    return _array_without_holes(arr) || _iterable_to_array(arr) || _unsupported_iterable_to_array(arr) || _non_iterable_spread();
-}
-function _unsupported_iterable_to_array(o, minLen) {
-    if (!o) return;
-    if (typeof o === "string") return _array_like_to_array(o, minLen);
-    var n = Object.prototype.toString.call(o).slice(8, -1);
-    if (n === "Object" && o.constructor) n = o.constructor.name;
-    if (n === "Map" || n === "Set") return Array.from(n);
-    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
-}
-function parseFlags(r) {
-    var a = new Map();
-    if (!r) return a;
-    var t = /\s-([a-zA-Z]+)(?:=(\S*))?/g;
-    var e = _to_consumable_array(r.matchAll(t));
-    e.forEach(function(r) {
-        return a.set(r[1], r[2] || 'true');
-    });
-    return a;
-}
-function parseString(a) {
-    var e = /^(?:([0-9]+)\s+)?(.*?)((?:\s-[a-zA-Z]+(?:=\S*)?)*)?$/;
-    var t = a.match(e);
-    if (t == null) return undefined;
-    var n = parseInt(t[1]);
-    var s = isNaN(n) ? 1 : n;
-    var u = parseFlags(t[3]);
-    var i = u.get("l");
-    u.delete("l");
+function parseMTGCardString(n) {
+    const e = /^(?:([0-9]+)\s+)?(.*?)((?:\s-[a-zA-Z]+(?:=\S*)?)*)?$/;
+    const s = n.match(e);
+    if (s == null) return undefined;
+    const a = parseInt(s[1]);
+    const c = isNaN(a) ? 1 : a;
+    const r = t(s[3]);
+    const o = r.get("l");
+    r.delete("l");
     return {
-        name: t[2],
-        quantity: s,
-        language: i,
-        customFlags: u
+        name: s[2],
+        quantity: c,
+        language: o,
+        customFlags: r
     };
+}
+function t(t) {
+    const n = new Map();
+    if (!t) return n;
+    const e = /\s-([a-zA-Z]+)(?:=(\S*))?/g;
+    const s = [
+        ...t.matchAll(e)
+    ];
+    s.forEach((t)=>n.set(t[1], t[2] || 'true'));
+    return n;
+}
+const a = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+const e = /^[a-zA-Z0-9]{3,5}\/[^/]+(?:\/(?:[a-zA-Z]{2,3})?)?$/;
+const t1 = [
+    'en',
+    'es',
+    'fr',
+    'de',
+    'it',
+    'pt',
+    'ja',
+    'ko',
+    'ru',
+    'zhs',
+    'zht'
+];
+function s(a) {
+    return new Promise((e)=>{
+        setTimeout(e, a);
+    });
+}
+async function getCard(n) {
+    let s;
+    if (a.test(n.name)) s = `https://api.scryfall.com/cards/${n.name}`;
+    else if (e.test(n.name)) {
+        let a = n.name;
+        if (n.language != undefined && t1.includes(n.language)) a = n.name.split('/').slice(0, 2).join('/') + `/${n.language}`;
+        s = `https://api.scryfall.com/cards/${a}`;
+    } else s = `https://api.scryfall.com/cards/named/?fuzzy=${encodeURIComponent(n.name)}`;
+    const c = fetch(s);
+    const o = await c;
+    return await o.json();
+}
+async function getCardsBatch(a) {
+    return await Promise.all(a.map((a, e)=>s(e * 100).then(()=>getCard(a)))).then((a)=>a.flat());
 }
