a9bb09c6efa5ebe83c040ae410abc3cf86eab456
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-10-19 11:50:01 +0200

    First version of getCardsPrint

--- src/services/scryfall.ts
+++ src/services/scryfall.ts
@@ -40,13 +40,13 @@ export async function getCard(
 
   if (uuidRegexp.test(parsedCard.name)) {
     // Name is a UUID, so we request the direct endpoint
-    requestURI = `https://api.scryfall.com/cards/${parsedCard.name}`;
+    requestURI = `${SCRYFALL_API_URI}/cards/${parsedCard.name}`;
   } else if (codeNumberRegexp.test(parsedCard.name)) {
     // Name is a code, so we request the direct endpoint with modifications
     // for the language
     let cardCode = parsedCard.name;
     if (
-      parsedCard.language != undefined &&
+      parsedCard.language != null &&
       validLanguages.includes(parsedCard.language)
     ) {
       cardCode =
@@ -66,7 +66,33 @@ export async function getCard(
   if (res.ok) {
     return await res.json();
   } else {
-    return undefined;
+    return Promise.resolve(undefined);
+  }
+}
+
+/* Given a ScryfallCard, return all prints of tha card.
+ * DO NOT call this function repetitively as it is not buffered and could lead
+ * you to be blocked by Scryfall.
+ * Instead use the dedicated getCardPrintsAndDo() function.
+ */
+async function getCardPrintsFromCard(
+  scryfallCard: ScryfallCard,
+  includeMultilingual = false,
+  onlyPaper = true
+): Promise<ScryfallCard[] | undefined> {
+  const url = includeMultilingual
+    ? `${scryfallCard.prints_search_uri}&include_multilingual=true`
+    : scryfallCard.prints_search_uri;
+  const res: Response = await fetch(url);
+  if (res.ok) {
+    return await res
+      .json()
+      .then((json) => json.data)
+      .then((cards: ScryfallCard[]) =>
+        onlyPaper ? cards.filter((card) => card.games.includes('paper')) : cards
+      );
+  } else {
+    return Promise.resolve(undefined);
   }
 }
 
@@ -92,10 +118,7 @@ export async function getCardsBatch(
  */
 export async function getCardsAndDo<T extends ParsedCard>(
   cards: T[],
-  callback: (
-    input: T,
-    foundCard: ScryfallCard | undefined
-  ) => void
+  callback: (input: T, foundCard: ScryfallCard | undefined) => void
 ): Promise<void> {
   const promises: Promise<void>[] = [];
   cards.forEach((card, index) => {
@@ -107,6 +130,44 @@ export async function getCardsAndDo<T extends ParsedCard>(
         customFlags: card.customFlags,
       }).then((scryfallCard) => callback(card, scryfallCard))
     );
+    promises.push(promise);
+  });
+  return Promise.all(promises).then();
+}
+
+/* This function calls Scryfall with delay to fetch all prints of
+ * the given `cards` and calls `callback` for each one with the input and the
+ * result from Scryfall (undefined if no card was found or an error occurred).
+ */
+
+export async function getCardPrintsAndDo<T extends ParsedCard>(
+  cards: T[],
+  callback: (input: T, foundCards: ScryfallCard[] | undefined) => void,
+  includeMultilingual = false,
+  onlyPaper = true
+): Promise<void> {
+  const promises: Promise<void>[] = [];
+  cards.forEach((card, index) => {
+    const promise = _later(index * API_DELAY_MS)
+      .then(() =>
+        getCard({
+          name: card.name,
+          quantity: card.quantity,
+          language: card.language,
+          customFlags: card.customFlags,
+        })
+      )
+      .then(
+        (scryfallCard): Promise<ScryfallCard[] | undefined> =>
+          scryfallCard !== undefined
+            ? getCardPrintsFromCard(
+                scryfallCard,
+                includeMultilingual,
+                onlyPaper
+              )
+            : Promise.resolve(undefined)
+      )
+      .then((scryfallPrints) => callback(card, scryfallPrints));
     promises.push(promise);
   });
   return Promise.all(promises).then();
