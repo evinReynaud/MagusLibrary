07b74d29db492b92c859399531cd96c16397c3be
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-11-01 16:02:56 +0100

    Update Scryfall types

--- src/types/parsed-card.ts
+++ src/types/parsed-card.ts
@@ -1,6 +1,6 @@
 export type ParsedCard = {
   readonly name: string;
   readonly quantity: number;
-  readonly language: string | undefined;
+  readonly language?: string;
   readonly customFlags: ReadonlyMap<string, string>; // Found flags are mapped to their data, or 'true' for dataless flags
 };
